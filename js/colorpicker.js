3f10320b1928f506eaf991a00d5c874025929e70
Atif Farooq <atifarooq86@gmail.com>
2020-06-14 09:33:03 +0500

    set visibility, object selection, color changer

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -24,12 +24,46 @@ var sliderPicker = new iro.ColorPicker(".colorPicker", {
                 sliderType: 'value'
             }
         },
+        // {
+        //     component: iro.ui.Slider,
+        //     options: {
+        //         sliderType: 'alpha'
+        //     }
+        // },
+    ]
+});
+
+var controlColorPicker = new iro.ColorPicker(".controlColorPicker", {
+    width: 250,
+    color: "#d93bbf",
+    padding: 0,
+    handleRadius:5,
+    borderWidth: 0,
+    layout: [
         {
             component: iro.ui.Slider,
             options: {
-                sliderType: 'alpha'
+                sliderType: 'hue'
             }
         },
+        {
+            component: iro.ui.Slider,
+            options: {
+                sliderType: 'saturation'
+            }
+        },
+        {
+            component: iro.ui.Slider,
+            options: {
+                sliderType: 'value'
+            }
+        },
+        // {
+        //     component: iro.ui.Slider,
+        //     options: {
+        //         sliderType: 'alpha'
+        //     }
+        // },
     ]
 });
 
@@ -37,6 +71,7 @@ var sliderPicker = new iro.ColorPicker(".colorPicker", {
 var body = document.querySelector("body");
 
 sliderPicker.on(["color:init", "color:change"], color => {
-
     body.style.backgroundColor = color.hexString;
 });
+
+export default controlColorPicker;
